c4dbce81c460c167f12e2cbedfbf1ab3fa66011d
あんどうしょう <sho@andoshonoMacBook-Air-2.local>
2023-01-20 09:54:06 +0900

    print-kidsのダウンロード・プリントshell を追加

---
+++ print-kids/download.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+## easy
+for i in {1..26}
+do
+  wget -O nazorigaki/nazorigaki-easy${i}.pdf https://print-kids.net/print/unpitsu/nazorigaki/nazorigaki-easy${i}.pdf
+done
+
+## nomal
+for i in {1..32}
+do
+  wget -O nazorigaki/nazorigaki-normal${i}.pdf https://print-kids.net/print/unpitsu/nazorigaki/nazorigaki-normal${i}.pdf
+done
---
+++ print-kids/print.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+lpr -P Brother_MFC_J6583CDW nazorigaki/nazorigaki-*
