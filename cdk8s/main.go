418e886600affd4bb06a9b77cd1d3998253953ab
anderson <butter.c.anderson@outlook.com>
2023-06-24 22:44:24 +0900

    Ingressがcdk8sで上手く書けないのでk8sで追加

--- cdk8s/main.go
+++ cdk8s/main.go
@@ -165,6 +165,52 @@ func NewArgocd(scope constructs.Construct, id string, props *MyChartProps) cdk8s
 	return chart
 }
 
+func NewArgoWorkflows(scope constructs.Construct, id string, props *MyChartProps) cdk8s.Chart {
+	var cprops cdk8s.ChartProps
+	if props != nil {
+		cprops = props.ChartProps
+	}
+	chart := cdk8s.NewChart(scope, jsii.String(id), &cprops)
+
+	// define resources here
+	cdk8s.NewHelm(chart, jsii.String(id), &cdk8s.HelmProps{
+		Namespace: jsii.String(id),
+		Chart: jsii.String("argo/argo-workflows"), //helm repo add argo https://argoproj.github.io/argo-helm
+	})
+
+	k8s.NewKubeIngress(chart, jsii.String(id + "-ingress"), &k8s.KubeIngressProps{
+	    Metadata: &k8s.ObjectMeta{
+	        Name: jsii.String(id),
+	    },
+	    Spec: &k8s.IngressSpec{
+	        IngressClassName: jsii.String("nginx"),
+	        Rules: &[]*k8s.IngressRule{
+	            {
+	                Host: jsii.String(id + ".localhost"),
+	                Http: &k8s.HttpIngressRuleValue{
+	                    Paths: &[]*k8s.HttpIngressPath{
+	                        {
+	                            Path:     jsii.String("/"),
+	                            PathType: jsii.String("Prefix"),
+	                            Backend: &k8s.IngressBackend{
+	                                Service: &k8s.IngressServiceBackend{
+	                                    Name: jsii.String(id),
+	                                    Port: &k8s.ServiceBackendPort{
+	                                        Number: jsii.Number(2746),
+	                                    },
+	                                },
+	                            },
+	                        },
+	                    },
+	                },
+	            },
+	        },
+	    },
+	})
+
+	return chart
+}
+
 func NewNginx(scope constructs.Construct, id string, props *MyChartProps) cdk8s.Chart {
 	var cprops cdk8s.ChartProps
 	if props != nil {
@@ -203,6 +249,7 @@ func main() {
 	NewDeploymentUbuntu(app, "ubuntu", nil)
 	NewRoleBinding(app, "rolebinding", nil)
 	NewArgocd(app, "argocd", nil)
+	NewArgoWorkflows(app, "argo-workflows", nil)
 	NewNginx(app, "nginx", nil)
 	NewIngressNginx(app, "ingress-nginx-controller", nil)
 	app.Synth()
