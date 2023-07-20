a6a231e04fdb6bd20b7b28201e7a9c4ba721f32f
anderson <butter.c.anderson@outlook.com>
2023-07-20 23:07:40 +0900

    geth作成中

---
+++ cdk8s/geth.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"github.com/aws/constructs-go/constructs/v10"
+	"github.com/aws/jsii-runtime-go"
+	"github.com/cdk8s-team/cdk8s-core-go/cdk8s/v2"
+	"github.com/cdk8s-team/cdk8s-plus-go/cdk8splus26/v2"
+)
+
+func NewDeploymentGeth(scope constructs.Construct, id string, props *MyChartProps) cdk8s.Chart {
+	var cprops cdk8s.ChartProps
+	if props != nil {
+		cprops = props.ChartProps
+	}
+	chart := cdk8s.NewChart(scope, jsii.String(id), &cprops)
+
+	// define resources here
+	// localnet
+	replicas:= float64(1.0)
+
+	cdk8splus26.NewDeployment(chart, jsii.String(id), &cdk8splus26.DeploymentProps{
+		Metadata: &cdk8s.ApiObjectMetadata{
+			Name: jsii.String(id),
+		},
+		Replicas: &replicas,
+		Containers: &[]*cdk8splus26.ContainerProps{
+			{
+				Name: jsii.String(id),
+				Image: jsii.String("ethereum/client-go:latest"),
+				Args: &[]*string{
+					jsii.String("--ipcdisable"),
+					jsii.String("--rpc"),
+				},
+			},
+		},
+	})
+
+	return chart
+}
