32a9139c8618858e5463d226f24ab6538ba53f1d
anderson <butter.c.anderson@outlook.com>
2025-06-27 23:09:57 +0900

    start rust

---
+++ rust/hello/target/debug/deps/hello-b2ef320b08bcd145.d
@@ -0,0 +1,5 @@
+/Users/sho/Git/sandbox/rust/hello/target/debug/deps/hello-b2ef320b08bcd145: src/main.rs
+
+/Users/sho/Git/sandbox/rust/hello/target/debug/deps/hello-b2ef320b08bcd145.d: src/main.rs
+
+src/main.rs:
---
+++ rust/hello/target/debug/hello.d
@@ -0,0 +1,1 @@
+/Users/sho/Git/sandbox/rust/hello/target/debug/hello: /Users/sho/Git/sandbox/rust/hello/src/main.rs
