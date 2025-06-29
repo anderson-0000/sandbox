2c68a76a214465f8a6e7e431a0b8c65c5061314e
anderson <butter.c.anderson@outlook.com>
2025-06-29 12:28:26 +0900

    add sin cos tan

--- R/various_shapes.R
+++ R/various_shapes.R
@@ -1,17 +1,29 @@
 options(repos = c(CRAN = "https://cloud.r-project.org"))
 
-install.packages("showtext")
-install.packages("Cairo")
-install.packages("ragg")
-install.packages("sysfonts")
-install.packages("curl")
+#install.packages("showtext")
+#install.packages("Cairo")
+#install.packages("ragg")
+#install.packages("sysfonts")
+#install.packages("curl")
 
-library(showtext)
-library(ragg)
-library(sysfonts)
+# パッケージリスト
+required_packages <- c("showtext", "Cairo", "ragg", "sysfonts", "curl")
+
+for (package_name in required_packages) {
+    if (!(package_name %in% installed.packages())) {
+        install.packages(package_name)
+    }
+}
+
+# パッケージのロード
+lapply(required_packages, require, character.only = TRUE)
+
+#library(showtext)
+#library(ragg)
+#library(sysfonts)
+
+# フォント指定
 font_add_google("Noto Sans JP", "notosans-jp")
-
-dev.new()  # グラフィックスウィンドウをクリア
 
 ## Rの対話モードの時だけ
 #showtext_auto()
@@ -46,6 +58,26 @@ symbols(7, 7, circles = 1, inches = FALSE, add = TRUE, bg = "pink")
 
 # 三角形
 lines(c(2, 8, 5, 2), c(2, 2, 8, 2), col="red", lwd=2)
+
+# y=4
+abline(h = 4, col = "gray", lty = 2)
+
+# y=sin(x)
+lines(seq(0, 10), sin(seq(0, 10)), col = "blue", lwd = 1)
+lines(seq(0, 10, length.out = 100), sin(seq(0, 10, length.out = 100)), col = "blue", lwd = 1)
+# y=sin(x)+4
+lines(seq(0, 10, length.out = 100), sin(seq(0, 10, length.out = 100)) + 4, col = "blue", lwd = 1)
+
+# y=cos(x)
+lines(seq(0, 10, length.out = 100), cos(seq(0, 10, length.out = 100)), col = "forestgreen", lwd = 2)
+# y=cos(x)+4
+lines(seq(0, 10, length.out = 100), cos(seq(0, 10, length.out = 100)) + 4, col = "forestgreen", lwd = 2)
+
+# y=tan(x)
+lines( seq(0, 10, length.out = 100), ifelse(abs(tan(seq(0, 10, length.out = 100))) > 10, NA, tan(seq(0, 10, length.out = 100))), col = "red", lwd = 2 )
+# y=tan(x)+4
+lines( seq(0, 10, length.out = 2000), ifelse(abs(tan(seq(0, 10, length.out = 2000)) + 4) > 10, NA, tan(seq(0, 10, length.out = 2000)) + 4), col = "red", lwd = 2 )
+
 
 # グリッド線を追加
 grid()
